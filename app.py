b53addda08671e4ce2c61416d27eb026cda5a02b
Apurba Pandey <apand024@uottawa.ca>
2022-10-21 10:48:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    result = True
     recommended_books = list()
     bookname = str()
     if request.method == 'POST':
         bookname = request.form.get('bookname')
         recommended_books = similar_books(bookname,b_weights)[1:]
-    return render_template('index.html', recommended_books=recommended_books, bookname=bookname)
+        result = False if not recommended_books else True
+    return render_template('index.html', recommended_books=recommended_books, bookname=bookname, result=result)
 
 
 if __name__ == '__main__':
-    app.run(debug = False, host = '0.0.0.0')
+    app.run()
